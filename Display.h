fa41752c4ce5b9f74e7a352d77c85500ca9cacaf
ofrika <35461126+ofrika@users.noreply.github.com>
2019-11-03 13:00:59 +0200

    With changes - draw now

--- Display.h
+++ Display.h
@@ -1,14 +1,16 @@
-// Created by Anabusy on 8/29/2019.
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
 #include <stdbool.h>
 
+
+// ************** Data structure handling function declerations *******************
+
+
 typedef unsigned char byte;
 
 typedef enum ledSignResult_t LedSignResult;
 typedef enum Direction_t{UP, DOWN, LEFT, RIGHT, UNDEFINED_DIR} Direction;
-
 
 enum ledSignResult_t {
     LED_SIGN_SUCCESS,
@@ -31,7 +33,7 @@ enum ledSignResult_t {
 };
 
 
-// ************** Data structure handling function declerations *******************
+// ************** Function declerations - general board handling *******************
 
 // Allocates space for board struct, fills in screen size values, creates 2 empty screen variables
 LedSignResult initBoard(int numPorts, int* ports, char* directions);
@@ -39,43 +41,52 @@ LedSignResult initBoard(int numPorts, int* ports, char* directions);
 // Frees allocated memory for board
 void destroyBoard();
 
-// Allocates space for display struct, fills in size values, and adds it to sub-board list
-LedSignResult addSubBoard(int dispID, int x, int y, int lenX, int lenY);
+// Allocates space for sub-board struct, fills in size values, and adds it to sub-board list
+LedSignResult addSubBoard(int subBoardID, int x, int y, int lenX, int lenY);
 
-// deletes all the objects in the desired display
+// deletes all the objects in the desired sub-board
 LedSignResult cleanSubBoard(int subBoardID);
 
-// Frees allocated memory for disp and deletes from list
+// Frees allocated memory for sub-board and deletes from list
 LedSignResult deleteSubBoard(int subBoardID);
 
-// Adds Text to the adequate Display. return -1 if the location is Illegal
-LedSignResult createTextArea(int dispID, int TextID, int x, int y, int lenX, int lenY, byte r, byte g, byte b, bool scrollable);
-
-// Adds new image to the stock so the user can use "addPicture" operation
-LedSignResult addImageToStock(int imageID, int height, int width, byte* rData, byte* gData, byte* bData);
-
-// Adds picture to the adequate Display
-LedSignResult createPictureArea(int dispID, int pictureID, int x, int y, int lenX, int lenY, bool newColor, byte r, byte g, byte b);
-
-// updates the text in the given display and writes a new data
-LedSignResult updateText(int dispID, int textID, int* data, int data_len);
-
-// updates the text color 
-LedSignResult updateTextColor(int dispID, int textID, byte r, byte g, byte b);
-
-// update the picture in the given display and draw a new data
-LedSignResult updatePicture(int dispID, int pictureID, int newImgId);
-
-// updates the picture color 
-LedSignResult updatePictureColor(int dispID, int pictureID, byte r, byte g, byte b);
-
 // Frees allocated memory for obj and deletes from list
-LedSignResult deleteArea(int dispID, int areaID);
+LedSignResult deleteArea(int subBoardID, int areaID);
 
-// gets the current status of the system
+// Gets the current status of the system
 void getStatus();
 
 // Draws new screen on LED screen
 LedSignResult DrawBoard();
+
+
+// ************** Function declerations - text object handling *******************
+
+
+// Adds Text to the requested sub-board. Returns -1 if the location is Illegal
+LedSignResult createTextArea(int subBoardID, int TextID, int x, int y, int lenX, int lenY, byte r, byte g, byte b, bool scrollable);
+
+// Updates the text in the given sub-board and writes a new data
+LedSignResult updateText(int subBoardID, int textID, int* data, int data_len, bool draw);
+
+// Updates the text color 
+LedSignResult updateTextColor(int subBoardID, int textID, byte r, byte g, byte b, bool draw);
+
+
+// ************** Function declerations - picture object handling *******************
+
+
+// Adds picture to the requested sub-board
+LedSignResult createPictureArea(int subBoardID, int pictureID, int x, int y, int lenX, int lenY);
+
+// Adds new image to the database so the user can use "addPicture" operation
+LedSignResult addImageToDB(int imageID, int height, int width, byte* rData, byte* gData, byte* bData);
+
+// Updates the picture in the given sub-board and draw a new data
+LedSignResult updatePicture(int subBoardID, int pictureID, int index, bool draw);
+
+// Updates the picture color 
+LedSignResult updatePictureColor(int subBoardID, int pictureID, byte orig_r, byte orig_g, byte orig_b, byte new_r, byte new_g, byte new_b, bool draw);
+
 
 #endif
