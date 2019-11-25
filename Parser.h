dcbc3f1f22d297e06380684980347bd804af3787
ofrika <35461126+ofrika@users.noreply.github.com>
2019-11-25 20:05:41 +0200

    Latest parser header - working

--- Parser.h
+++ Parser.h
@@ -1,13 +1,13 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#ifndef PARSER_H
+#define PARSER_H
 
 #include <stdio.h>
 #include <string.h>
-#include "Display.h"
+#include <stdbool.h>
 
 #define BUFFSIZE 8192
 
-// Parses message and returns whether it was destroy board message and therefore loop should end
+// Parse message and return whether it was destroy board message and therefore loop should end
 int parseMessage(char* input);
 
 #endif
